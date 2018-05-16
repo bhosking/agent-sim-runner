5b10d5c2048fa778bf9d5f7abd801920e0f8ab7c
Brendan Hosking <brendan.hosking@gmail.com>
2018-05-16 22:30:14 +1000

    FEAT: Make Counter return individual tests

--- autorun.py
+++ autorun.py
@@ -6,10 +6,23 @@ import threading
 THREADS = 2
 TEST_SUFFIX = 'Brendan'
 
+TESTS = [
+    ('e2p0fl0r', []),
+    ('e2p0fl1r', ['-longevityLandscape=10000,0,1,0.5,0.5',
+                  '-fecundityLandscape=0.01,1,0,0.5,0.5']),
+    ('e2p1fl0r', ['-predationProbability=0.0002 ']),
+    ('e2p1fl1r', ['-predationProbability=0.0002',
+                  '-longevityLandscape=10000,0,1,0.5,0.5',
+                  '-fecundityLandscape=0.01,1,0,0.5,0.5'])
+]
+
 
 class Counter(object):
-    def __init__(self, value=0):
-        self.value = value
+    def __init__(self, tests, test_suffix, repeat=0, test_index=0):
+        self.tests = tests
+        self.repeat = repeat
+        self.test_index = test_index
+        self.test_suffix = test_suffix
         self.lock = threading.Lock()
 
     def __iter__(self):
@@ -17,9 +30,18 @@ class Counter(object):
 
     def __next__(self):
         with self.lock:
-            n = self.value
-            self.value += 1
-        return n
+            test_index = self.test_index
+            repeat = self.repeat
+            if test_index >= len(self.tests) - 1:
+                self.test_index = 0
+                self.repeat += 1
+            else:
+                self.test_index += 1
+            test = self.tests[test_index]
+            suffix = self.test_suffix
+        name = test[0] + suffix + str(repeat)
+        args = test[1]
+        return name, args
 
 
 def run_test(test_name, extra_args):
@@ -32,18 +54,11 @@ def run_test(test_name, extra_args):
 
 
 def run_tests(iterator):
-    for n in iterator:
-        suffix = TEST_SUFFIX + str(n)
-        run_test('e2p0fl0r' + suffix, [])
-        run_test('e2p0fl1r' + suffix, ['-longevityLandscape=10000,0,1,0.5,0.5',
-                                       '-fecundityLandscape=0.01,1,0,0.5,0.5'])
-        run_test('e2p1fl0r' + suffix, ['-predationProbability=0.0002 '])
-        run_test('e2p1fl1r' + suffix, ['-predationProbability=0.0002',
-                                       '-longevityLandscape=10000,0,1,0.5,0.5',
-                                       '-fecundityLandscape=0.01,1,0,0.5,0.5'])
+    for test_name, args in iterator:
+        run_test(test_name, args)
 
 
-counter = Counter()
+counter = Counter(TESTS, TEST_SUFFIX)
 for i in range(THREADS - 1):
     thread = threading.Thread(target=run_tests, kwargs={'iterator': counter})
     thread.start()
